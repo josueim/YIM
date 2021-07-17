35f63f855f25d2c4ef7a283c44b848889ee10a16
Josue Islas <unerjosue@gmail.com>
2021-07-17 00:46:18 -0500

    First commit - Header responsive creation

---
+++ Inicio/index.js
@@ -0,0 +1,15 @@
+/*const-var   navbar*/ 
+const navHamburger = document.querySelector(".navbar-hamburger")
+const navbarMenu = document.querySelector(".navbar-menu")
+
+/*Navbar - This way we're making a responsive navbar*/ 
+navHamburger.addEventListener("click", () =>{
+    navbarMenu.classList.toggle("navbar-menu_unvisible");
+
+    /*This way we make sure our page it´s accesible for everyone*/
+    if(navbarMenu.classList.contains("navbar-menu_visible")){
+        navHamburger.setAttribute("aria-label", "Cerrar menú");
+    }else{
+        navHamburger.setAttribute("aria-label", "Abrir menú");    
+    }
+});
