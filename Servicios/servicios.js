bb8268f8d31f3e874a4e3242e987a6eef290572c
Josue Islas <unerjosue@gmail.com>
2021-07-29 21:53:24 -0500

    slider buttons fix - top responsive servicios creation

--- Servicios/servicios.js
+++ Servicios/servicios.js
@@ -1,14 +1,10 @@
 /*const-var   navbar*/ 
 const navHamburger = document.querySelector(".navbar-hamburger")
 const navbarMenu = document.querySelector(".navbar-menu")
-const sliderVisible = document.querySelector('#slider-visble')
-const buttonsVisible = document.querySelector('#buttons-visible')
  
 /*Navbar - This way we're making a responsive navbar*/ 
 navHamburger.addEventListener("click", () =>{
     navbarMenu.classList.toggle("navbar-menu_unvisible");
-    sliderVisible.classList.toggle("slider__visible");
-    buttonsVisible.classList.toggle("buttons__visible");
 
     /*This way we make sure our page it´s accesible for everyone*/
     if(navbarMenu.classList.contains("navbar-menu_visible")){
